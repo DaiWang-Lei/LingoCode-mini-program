6c6be2aa683a52bd9ab8434bf1df73fff8fc85b0
代罔 <1403693160@qq.com>
2019-12-27 11:52:20 +0800

    comment done

---
+++ comments/lingo.js
@@ -0,0 +1,34 @@
+// pages/lingo/ling o.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:{
+      type:String,
+      value:''
+    },
+    progress:{
+      type:String,
+      value:'已完成步骤0/2'
+    },
+    theDay:{
+      type:String,
+      value:''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ pages/more/about.js
@@ -0,0 +1,70 @@
+// pages/more/about.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: 'LingoCode凌高编程',
+      desc: '「凌高编程」专为青少年打造的入门级编程。让您的孩子从此以后，知识无断层，从此爱编程',
+      path: '/pages/index/index'
+    }
+  }
+})
